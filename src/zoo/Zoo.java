4add83dac3617b2aa4fbae9537ac7a1410086742
genmax123 <genrichm867@gmail.com>
2025-12-28 13:19:49 +0100

    Test Comit from Max

--- src/zoo/Zoo.java
+++ src/zoo/Zoo.java
@@ -2,20 +2,38 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package zoo;
-
+package zoo; 
+import java.util.Scanner;
 /**
  *
  * @author Maximillian Genrich
  */
 public class Zoo {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
         
+    boolean running = true; // bool Variable festgelegt --> wert auf wahr gestellt
+    
+    while (running){ //While schleife der Switch Case
+        System.out.println("Willkommen im Zoo GolzschGenrich GmbH"); // 21-30 Textprint für auswahlmenü
+        System.out.println("Wählen sie eine Option aus");
+        System.out.println("1. Biom wählen");
+        System.out.println("2. Anreise wählen");
+        System.out.println("3. Events wählen");
+        System.out.println("4. Warenkorb anzeigen");
+        System.out.println("5. Bezahlen und Ticket anzeigen");
+        System.out.println("6. Programm beenden");
+        System.out.println("7. About");
+        
+        int choice =Scanner.nextInt();
+        
+        
+        
+        
+    }
     
     }
     
